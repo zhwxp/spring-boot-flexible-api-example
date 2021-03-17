e23e533b371aafc736ca32e38d31f7bdb147a9bb
wayne.zhangtj <Wayne.ZhangTJ@homecredit.cn>
2021-03-17 11:21:02 +0800

    add pageable support

--- src/main/java/cn/zhwxp/spring/boot/flexible/api/example/controller/EmployeeController.java
+++ src/main/java/cn/zhwxp/spring/boot/flexible/api/example/controller/EmployeeController.java
@@ -1,12 +1,13 @@
 package cn.zhwxp.spring.boot.flexible.api.example.controller;
 
-import java.util.List;
-
+import net.kaczmarzyk.spring.data.jpa.domain.Between;
 import net.kaczmarzyk.spring.data.jpa.domain.In;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.Join;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,16 +29,18 @@ public class EmployeeController {
     }
 
     @GetMapping(value = "employees", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Employee> searchEmployeesBySpecification(
+    public Page<Employee> searchEmployeesBySpecification(
             @Join(path = "department", alias = "department")
             @And({
                     @Spec(path = "name", params = "name", paramSeparator = ',', spec = In.class),
                     @Spec(path = "gender", params = "gender", paramSeparator = ',', spec = In.class),
                     @Spec(path = "age", params = "age", paramSeparator = ',', spec = In.class),
+                    @Spec(path = "age", params = { "ageStart", "ageEnd" }, spec = Between.class),
                     @Spec(path = "department.name", params = "department.name", paramSeparator = ',', spec = In.class)
-            }) Specification<Employee> specification
+            }) Specification<Employee> specification,
+            Pageable pageable
     ) {
-        return employeeRepository.findAll(specification);
+        return employeeRepository.findAll(specification, pageable);
     }
 
 }
